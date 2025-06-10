4d10802d916691e8c407b8bbae3160d7519489be
wukaipeng <wkpcoder@163.com>
2025-06-11 00:17:34 +0800

    Fix import statement for useRouter in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-// import { useRouter } from 'next/navigation';
-import { useRouter } from '@bprogress/next/app';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function LoginPage() {
